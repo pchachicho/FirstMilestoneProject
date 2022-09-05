663352e6e5903a1a03cc715ea2a28f51476677aa
Patrick hachicho <Hachichopatrick@gmail.com>
2022-09-05 18:26:56 -0400

    added the canvas

---
+++ Map.js
@@ -0,0 +1,11 @@
+
+// Creates canvas and links it to the ID on html "game"
+const canvas = document.getElementById('game');
+// References the context in 2d 
+const ctx = canvas.getContext('2d');
+// Game loop draws the screen multiple times depending on what i put
+function gameLoop(){
+
+}
+canvas.width = 600;
+canvas.height = 500;
