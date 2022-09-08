1180d7dec76e48cf5e2229768998ae798be74457
Patrick hachicho <Hachichopatrick@gmail.com>
2022-09-07 20:26:22 -0400

    fixed bug where canvas was not showing

--- Game.js
+++ Game.js
@@ -1,14 +1,16 @@
 import TileMap from "./Tilemap.js";
+
+const tileSize = 32;
 // Creates canvas and links it to the ID on html "game"
 const canvas = document.getElementById("game");
 // References the context in 2d
 const ctx = canvas.getContext("2d");
-const tileSize = 32;
 const tileMap = new TileMap(tileSize);
 // Game loop draws the screen multiple times depending on what i put
 function gameLoop() {
-  tileMap.draw();
+  tileMap.draw(ctx);
 }
+tileMap.setCanvasSize(canvas);
 // 1000 = 1 second so the function calls gameLoop 75 times per second
 setInterval(gameLoop, 1000 / 75);
 
--- Tilemap.js
+++ Tilemap.js
@@ -1,22 +1,33 @@
 // t
 export default class TileMap {
   constructor(tileSize) {
-    this.tileSize;
+    this.tileSize = tileSize;
+
+    this.yellowDot = new Image();
+    this.yellowDot.src = "./images/yellowDot.png";
+
+    this.wall = new Image();
+    this.wall.src = "./images/wall.png";
   }
-  // Bricks = 1 Yellow = 2
+
+  // Bricks = 1 Yellow dot = 2
   map = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
-  draw() {
-   // console.log("draw");
+  draw(ctx) {}
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
   }
 }
