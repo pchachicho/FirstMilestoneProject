379230194ae974ce767449e533465fd9dfe7918d
Patrick hachicho <Hachichopatrick@gmail.com>
2022-09-08 18:07:47 -0400

    starting to add movement

--- Pacman.js
+++ Pacman.js
@@ -6,6 +6,11 @@ export default class Pacman {
     this.tileSize = tileSize;
     this.velocity = velocity;
     this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
     this.#loadPacmanImages();
   }
   draw(ctx) {
@@ -40,5 +45,17 @@ export default class Pacman {
     ];
 
     this.pacmanImageIndex = 0; //First image pacman starts as (fully closed)
+  }
+
+  #keydown =(event)=>{
+    // Up key
+    if(event.keyCode == 38){} 
+    // Down key
+    if(event.keyCode == 40){}
+    // Left key
+    if(event.keyCode == 37){}
+    // Right key
+    if(event.keyCode == 39){}
+
   }
 }
